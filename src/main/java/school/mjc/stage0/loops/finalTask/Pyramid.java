4c389d40ef70b65008d7288d1520676a644970a6
mary <laguta113@gmail.com>
2023-11-25 13:44:08 +0600

    Commit with a solution

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int row = 1; row <= cathetusLength; row++) {
+            for (int space = 1; space <= cathetusLength - row; space++) {
+                System.out.print(" ");
+            }
+            for (int dec = row; dec >= 1; dec--) {
+                System.out.print(dec);
+            }
+            for (int inc = 2; inc <= row; inc++) {
+                System.out.print(inc);
+            }
+            System.out.println();
+        }
 
     }
 
